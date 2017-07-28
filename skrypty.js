9ce57aaa55d791a73d9131913065ab1c1da1dc0c
Agnieszka Bocian <agn.bocian@gmail.com>
2017-07-28 11:10:22 +0200

    new web page

---
+++ skrypty.js
@@ -0,0 +1,11 @@
+function aktywnySkrypt() {
+	console.log("hej");
+}
+
+function pokazMenu(){
+	document.querySelector(".menu-mobilne").style.display="none";
+}
+
+function schowajMenu(){
+		console.log("jo");
+}
